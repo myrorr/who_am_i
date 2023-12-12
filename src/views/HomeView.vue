3c0d20d25f356b89e762f130062c99b797bf621c
myrorr <kustra.bence98@gmail.com>
2023-12-12 08:44:16 +0100

    chore: image path work

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,7 +12,7 @@ const carouselSlides : string[] = ['fuji_cuddle', 'fuji_sit', 'fuji_upside_down'
     <Carousel class="relative max-h-screen h-screen" v-slot="{currentSlide}">
       <Slide v-for="(slide, index) in carouselSlides" :key="index">
         <div v-show="currentSlide === index + 1" class="absolute top-0 left-0 w-full max-h-full h-full">
-          <img :src="`/${slide}.jpg`" :id="`img-${index + 1}`" class="w-full h-full object-contain">
+          <img :src="`./${slide}.jpg`" :id="`img-${index + 1}`" class="w-full h-full object-contain">
         </div>
       </Slide>
     </Carousel>
